27e7da9793cb2a165650fb3c145890f0d1aca2c0
skril <skril@DESKTOP-C9875AO>
2023-01-13 12:03:35 +0100

    No funca

---
+++ src/Ejercicio1.java
@@ -0,0 +1,39 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author skril
+ */
+public class Ejercicio1 {
+    
+    
+    
+    public static void main(String[] args) {
+        Scanner tcl= new Scanner(System.in);
+        int valor=0;
+        boolean leido=false;
+        
+        do{
+            try{
+                System.out.println("Introduce un numero entero: ");
+                valor=tcl.nextInt();
+                leido=true;
+            }catch (InputMismatchException e){
+                System.out.println("Error en la introduccion del numero");
+                tcl.nextLine();
+            }
+            
+        }while (!leido);
+        System.out.println("Hemos leido : "+valor);
+            
+        }
+        
+    }
+    
+
